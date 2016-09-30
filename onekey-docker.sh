7e4876981d73917649a17e578b54408605ba219d
gucl <gucl@asiainfo.com>
2016-09-30 16:22:06 +0800

    onekey 文件格式调整

--- onekey-docker.sh
+++ onekey-docker.sh
@@ -1,4 +1,4 @@
-git pull
-gradle clean && gradle build -x test
+git pull 
+gradle clean && gradle build -x test 
 docker build -t 10.19.13.18:5000/general-uac-web:v1.0 .   
-docker push 10.19.13.18:5000/general-uac-web:v1.0
+docker push 10.19.13.18:5000/general-uac-web:v1.0 
